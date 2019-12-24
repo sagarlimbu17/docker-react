d5deb89dfd9d9d4a29a0120bba90bed56a88c10e
slimbu <sagar.limbu@infodevelopers.com.np>
2019-12-24 16:40:11 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Excellent Job
         </a>
       </header>
     </div>
